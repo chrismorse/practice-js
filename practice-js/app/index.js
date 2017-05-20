4c4efc2d0f504ad2bfa3d2c42cf797319969ca2c
chrismorse <github@beeksma.com>
2017-05-19 17:58:59 -0700

    react working

---
+++ practice-js/app/index.js
@@ -0,0 +1,75 @@
+﻿var React = require('react');
+var ReactDOM = require('react-dom');
+require('./index.css');
+
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                Hello World!
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
+
+
+
+class HelloUser extends React.Component {
+    render() {
+        return (
+            <div>Hello, {this.props.name}</div>
+        )
+    }
+}
+
+ReactDOM.render(<HelloUser name="Chris" />, document.getElementById('app'));
+
+
+
+class Avatar extends React.Component {
+    render() {
+        return (
+          <img src={this.props.img} />
+    )
+  }
+}
+
+class Label extends React.Component {
+    render() {
+        return (
+          <h1>Name: {this.props.name}</h1>
+    )
+  }
+}
+
+class ScreenName extends React.Component {
+    render() {
+        return (
+          <h3>Username: {this.props.username}</h3>
+    )
+  }
+}
+
+class Badge extends React.Component {
+    render() {
+        return (
+          <div>
+            <Avatar img={this.props.user.img}/>
+            <Label name={this.props.user.name} />
+            <ScreenName username={this.props.user.username}/>
+          </div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <Badge user={{
+name: 'Tyler McGinnis',
+img: 'https://avatars0.githubusercontent.com/u/2933430?v=3&s=460',
+username: 'tylermcginnis'
+}} />,
+document.getElementById('app')
+);
