1016495982e16a6b12dc294b45ebd4c3e6cae8d3
chrismorse <github@beeksma.com>
2017-05-23 11:36:21 -0700

    map and filter together

--- practice-js/app/index.js
+++ practice-js/app/index.js
@@ -27,7 +27,55 @@ class HelloUser extends React.Component {
 
 ReactDOM.render(<HelloUser name="Chris" />, document.getElementById('app'));
 
+//-----------------------------------------------------------------------------------------
+// map and filter together.
 
+class Users extends React.Component {
+    render() {
+        return (
+          <div>
+            <h1>Friends</h1>
+            <ul>
+                {this.props.list.filter(function(friend) {
+                    return friend.friend === true;
+                }).map(function(friend){
+                    return <li key={friend.name}> {friend.name}</li>
+                })}
+        {/*Create an <li> for every name in the list array who is also your friend*/}
+    </ul>
+        
+    <hr />
+        
+    <h1> Non Friends </h1>
+    <ul>
+        {this.props.list.filter(function(friend) {
+            return friend.friend != true;
+        }).map(function(friend){
+            return <li key={friend.name}> {friend.name}</li>
+        })}
+    {/*Create an <li> for every name in the list array who is NOT your friend*/}
+    </ul>        
+  </div>
+      )
+  }
+}
+
+ReactDOM.render(
+  <Users list={[
+{ name: 'Tyler', friend: true },
+{ name: 'Ryan', friend: true },
+{ name: 'Michael', friend: false },
+{ name: 'Mikenzi', friend: false },
+{ name: 'Jessica', friend: true },
+{ name: 'Dan', friend: false } ]} 
+  />,
+  document.getElementById('app')
+);
+
+
+//-----------------------------------------------------------------------------------------
+// Avatar
+//-----------------------------------------------------------------------------------------
 
 class Avatar extends React.Component {
     render() {
@@ -67,9 +115,10 @@ class Badge extends React.Component {
 
 ReactDOM.render(
   <Badge user={{
-name: 'Tyler McGinnis',
-img: 'https://avatars0.githubusercontent.com/u/2933430?v=3&s=460',
-username: 'tylermcginnis'
+name: 'Chris Morse',
+img: 'https://avatars3.githubusercontent.com/u/333303?v=3&s=460',
+username: 'chrismorse'
 }} />,
 document.getElementById('app')
 );
+
