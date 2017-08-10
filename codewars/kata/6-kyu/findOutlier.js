65e8497141d84055bcc9fdc1bc271c79ebdb99c0
Chris Morse <cjm@beeksma.com>
2017-08-09 21:45:42 -0700

    refactored findOutlier

--- codewars/kata/6-kyu/findOutlier.js
+++ codewars/kata/6-kyu/findOutlier.js
@@ -1,25 +1,8 @@
 // https://www.codewars.com/kata/5526fc09a1bbd946250002dc
 
 function findOutlier(integers){
+  var even = integers.filter(num => num % 2 === 0);
+  var odd = integers.filter(num => num % 2 !== 0);
 
-  var firstIsEven = integers[0] % 2 === 0; 
-  var secondIsEven = integers[1] % 2 === 0; 
-  var thirdIsEven = integers[2] % 2 === 0; 
-  
-  var isArrayEven = false;
-  
-  // we can tell if the array is mostly even or odd based on the first 3 values
-  if ((firstIsEven && secondIsEven) || (firstIsEven && thirdIsEven) || (secondIsEven && thirdIsEven))
-    isArrayEven = true;
-    
-  function getOutlier(element) {
-    if (isArrayEven) {
-      return element % 2 === 1 || element % 2 === -1;
-    } else {
-      return element % 2 === 0;
-    }
-  }
-  
-  return integers.find(getOutlier);
-
+  return even.length > 1 ? odd[0] : even[0]
 }
