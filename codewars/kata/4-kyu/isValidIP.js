20fde85a988efbfb9e6018c3d4a5e3199f5eb497
chrismorse <github@beeksma.com>
2017-08-10 09:37:04 -0700

    refactored isValidIP.js

--- codewars/kata/4-kyu/isValidIP.js
+++ codewars/kata/4-kyu/isValidIP.js
@@ -3,22 +3,14 @@
 function isValidIP(str) {
   parts = str.split('.');
 
-  // special good IP
-  if (str === '0.0.0.0')
-    return true
-
   // need 4 parts
   if (parts.length != 4)
     return false;
 
   for (var i = 0; i < parts.length; i++) {
     
-    // can't start with a zero or start or end with a blank
-    if ((parts[i][0] === '0' && parts[i].length > 1 ) || parts[i][0] === ' ' || parts[i][parts[i].length - 1] === ' ')
-      return false;
-
-    // has to be a number
-    if (isNaN(parts[i]))
+    // check for weirdness like leading spaces, etc..
+    if (String(Number(parts[i])) !== parts[i])
       return false;
      
     // needs to be a number between 0 & 256
