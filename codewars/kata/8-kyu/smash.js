07f3c9c3f52c6131b6e3b1445479abc58d17e01b
chrismorse <github@beeksma.com>
2017-09-20 13:27:13 -0700

    smash refactored

--- codewars/kata/8-kyu/smash.js
+++ codewars/kata/8-kyu/smash.js
@@ -1,9 +1,4 @@
-// 
+// https://www.codewars.com/kata/sentence-smash
 function smash (words) {
-  return words.reduce((sentance, word, i) => {
-    sentance += word;
-    if (i !== words.length - 1)
-      sentance += " ";
-    return sentance;
-  },"");
+  return words.join(" ");
 };
