ec3eed6d5cea66a4816c171cee281fd418b9b67c
Chris Morse <github@beeksma.com>
2018-09-05 15:39:35 -0700

    multiple of 3 or 5

---
+++ codewars/kata/6-kyu/multipleof3or5.js
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/multiples-of-3-or-5/train/javascript
+
+function solution(number){
+  let sum = 0;
+  for (let i = 1; i < number; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      sum = sum + i;
+    } 
+  }
+  return sum;
+}
